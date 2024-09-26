b5bdea1bea4f0d2b1dbff2f4a63a847475415861
jawaharm07 <jawaharmayandi@gmail.comm>
2024-09-26 11:25:01 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneApi.Models;
+using System.Collections.Generic;
+
+namespace OneApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OneApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        public string Name { get; set; }
+
+        [Range(0, 10000, ErrorMessage = "Price must be between 0 and 10,000.")]
+        public decimal Price { get; set; }
+
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")]
+        public string Description { get; set; }
+    }
+}
